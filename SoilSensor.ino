3a1f7b648aca4a99245103039aec16a713a01eab
Geemunz <Geemunz@users.noreply.github.com>
2020-04-14 18:12:41 -0400

    Add files via upload

---
+++ SoilSensor.ino
@@ -0,0 +1,56 @@
+/*  Soil Mositure Basic Example
+    This sketch was written by SparkFun Electronics
+    Joel Bartlett 
+    August 31, 2015
+
+    Basic skecth to print out soil moisture values to the Serial Monitor 
+
+    Released under the MIT License(http://opensource.org/licenses/MIT)
+*/
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int val = 0; //value for storing moisture value 
+int soilPin = A0;//Declare a variable for the soil moisture sensor 
+int soilPower = 7;//Variable for Soil moisture Power
+
+//Rather than powering the sensor through the 3.3V or 5V pins, 
+//we'll use a digital pin to power the sensor. This will 
+//prevent corrosion of the sensor as it sits in the soil. 
+
+void setup() 
+{
+  Serial.begin(9600);   // open serial over USB
+  lcd.begin(16,2);
+  pinMode(soilPower, OUTPUT);//Set D7 as an OUTPUT
+  digitalWrite(soilPower, LOW);//Set to LOW so no power is flowing through the sensor
+}
+
+void loop() 
+{
+lcd.clear();
+Serial.print("Soil Moisture = ");    
+lcd.print("Soil Mositure:");
+//get soil moisture value from the function below and print it
+Serial.print(readSoil());
+lcd.setCursor(0, 1);
+lcd.print(readSoil());
+lcd.print("%");
+Serial.println("%");
+
+
+//This 1 second timefrme is used so you can test the sensor and see it change in real-time.
+//For in-plant applications, you will want to take readings much less frequently.
+delay(10000);//take a reading every second
+}
+//This is a function used to get the soil moisture content
+int readSoil()
+{
+
+    digitalWrite(soilPower, HIGH);//turn D7 "On"
+    delay(400);//wait 10 milliseconds 
+    val = map(analogRead(soilPin), 580, 332, 0,100);//Read the SIG value form sensor 
+    digitalWrite(soilPower, LOW);//turn D7 "Off"
+    return val;//send current moisture value
+}
+
